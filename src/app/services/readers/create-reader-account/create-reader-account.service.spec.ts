999260da5112732b4278f2894b03b597ebef1ebf
caiogmrocha <caioguilherme739@gmail.com>
2024-06-25 19:01:17 -0300

    test(LIB-2): ensure CreateReaderAccountService is able to create a new reader account

--- src/app/services/readers/create-reader-account/create-reader-account.service.spec.ts
+++ src/app/services/readers/create-reader-account/create-reader-account.service.spec.ts
@@ -3,20 +3,30 @@ import { faker } from '@faker-js/faker';
 
 import { CreateReaderAccountService } from './create-reader-account.service';
 import { ReaderEmailAlreadyExistsException } from '../errors/reader-email-already-exists.error';
-import { ReaderRepository } from '../../interfaces/repositories/reader-repository';
-import { Reader } from '../../../domain/entities/reader';
+import { ReaderRepository } from '../../../interfaces/repositories/reader.repository';
+import { BcryptProvider } from '../../../interfaces/hash/bcrypt.provider';
+import { Reader } from '../../../../domain/entities/reader';
 
 describe('CreateReaderAccountService', () => {
   let service: CreateReaderAccountService;
   let readerRepository: jest.Mocked<ReaderRepository>;
+  let bcryptProvider: jest.Mocked<BcryptProvider>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
+          provide: BcryptProvider,
+          useClass: jest.fn().mockImplementation(() => ({
+            hash: jest.fn(),
+            compare: jest.fn(),
+          })),
+        },
+        {
           provide: ReaderRepository,
           useClass: jest.fn().mockImplementation(() => ({
-            getByEmail: jest.fn().mockResolvedValue({}),
+            getByEmail: jest.fn(),
+            create: jest.fn(),
           })),
         },
         CreateReaderAccountService,
@@ -25,23 +35,28 @@ describe('CreateReaderAccountService', () => {
 
     service = module.get<CreateReaderAccountService>(CreateReaderAccountService);
     readerRepository = module.get<jest.Mocked<ReaderRepository>>(ReaderRepository);
+    bcryptProvider = module.get<jest.Mocked<BcryptProvider>>(BcryptProvider);
   });
 
   it('should create a new reader account', async () => {
     // Arrange
-    readerRepository.getByEmail.mockResolvedValue(null);
-
     const params = {
       name: faker.person.fullName(),
       email: faker.internet.email(),
       password: faker.internet.password({ length: 12 }),
     };
 
+    readerRepository.getByEmail.mockResolvedValue(null);
+    bcryptProvider.hash.mockResolvedValue(params.password);
+
     // Act
-    await service.execute(params);
+    const promise = service.execute(params);
 
     // Assert
-    expect(readerRepository.getByEmail).resolves.toBeUndefined();
+    await expect(promise).resolves.toBeUndefined();
+    expect(readerRepository.getByEmail).toHaveBeenCalled();
+    expect(readerRepository.create).toHaveBeenCalled();
+    expect(bcryptProvider.hash).toHaveBeenCalled();
   });
 
   it('should throw ReaderEmailAlreadyExistsException when email already exists', async () => {
