9816a8353a4ce7f9b266a88bb01e054cacce7248
caiogmrocha <caioguilherme739@gmail.com>
2024-06-27 00:33:10 -0300

    feat(LIB-1): ensure AuthenticateReaderController response with 404 when reader does not exist

--- src/presentation/http/controllers/readers/authenticate-reader/authenticate-reader.controller.ts
+++ src/presentation/http/controllers/readers/authenticate-reader/authenticate-reader.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, NotFoundException, Post } from '@nestjs/common';
 
-@Controller('authenticate-reader')
-export class AuthenticateReaderController {}
+import { AuthenticateReaderDto } from './authenticate-reader.dto';
+import { AuthenticateReaderService } from '@/app/services/readers/authenticate-reader/authenticate-reader.service';
+import { ReaderNotFoundException } from '@/app/services/readers/errors/reader-not-found-exception.error';
+
+type AuthenticateReaderControllerResponse = {
+  token: string;
+};
+
+@Controller()
+export class AuthenticateReaderController {
+  constructor(
+    private readonly authenticateReaderService: AuthenticateReaderService
+  ) {}
+
+  @Post('/api/readers/login')
+  public async handle(@Body() requestDto: AuthenticateReaderDto): Promise<AuthenticateReaderControllerResponse> {
+    try {
+      const response = await this.authenticateReaderService.execute({
+        email: requestDto.email,
+        password: requestDto.password,
+      });
+
+      return response;
+    } catch (error) {
+      switch (error.constructor) {
+        case ReaderNotFoundException: {
+          throw new NotFoundException(error.message);
+        };
+      }
+    }
+  }
+}
