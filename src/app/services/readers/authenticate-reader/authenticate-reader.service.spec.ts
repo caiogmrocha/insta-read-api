e59b9f6298e133c2471a40dd5bfc639d81960ed7
caiogmrocha <caioguilherme739@gmail.com>
2024-06-26 23:53:46 -0300

    test(LIB-1): ensure AuthenticateReaderService throw ReaderNotFoundException when reader does not exist

--- src/app/services/readers/authenticate-reader/authenticate-reader.service.spec.ts
+++ src/app/services/readers/authenticate-reader/authenticate-reader.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { faker } from '@faker-js/faker';
+
 import { AuthenticateReaderService } from './authenticate-reader.service';
+import { ReaderNotFoundException } from '../errors/reader-not-found-exception.error';
+import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
 
 describe('AuthenticateReaderService', () => {
   let service: AuthenticateReaderService;
+  let readersRepository: jest.Mocked<ReadersRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticateReaderService],
+      providers: [
+        {
+          provide: ReadersRepository,
+          useClass: jest.fn().mockImplementation(() => ({
+            getByEmail: jest.fn(),
+            create: jest.fn(),
+          })),
+        },
+        AuthenticateReaderService
+      ],
     }).compile();
 
     service = module.get<AuthenticateReaderService>(AuthenticateReaderService);
+    readersRepository = module.get<jest.Mocked<ReadersRepository>>(ReadersRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should throw ReaderNotFoundException when reader does not exist', async () => {
+    // Arrange
+    const params = {
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 12 }),
+    };
+
+    readersRepository.getByEmail.mockResolvedValue(null);
+
+    // Act
+    const promise = service.execute(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(ReaderNotFoundException);
   });
+
+  it.todo('should throw InvalidReaderPasswordException when password does not match');
+  it.todo('should return a valid token when reader is authenticated');
 });
