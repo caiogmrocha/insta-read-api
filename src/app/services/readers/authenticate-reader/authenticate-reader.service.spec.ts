8e7e8c14b489a1749d945cbb6f37cac7ab12eec5
caiogmrocha <caioguilherme739@gmail.com>
2024-06-27 00:04:58 -0300

    test(LIB-1): ensure AuthenticateReaderService throw InvalidReaderPasswordException when password does not match

--- src/app/services/readers/authenticate-reader/authenticate-reader.service.spec.ts
+++ src/app/services/readers/authenticate-reader/authenticate-reader.service.spec.ts
@@ -5,10 +5,14 @@ import { faker } from '@faker-js/faker';
 import { AuthenticateReaderService } from './authenticate-reader.service';
 import { ReaderNotFoundException } from '../errors/reader-not-found-exception.error';
 import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
+import { Reader } from '@/domain/entities/reader';
+import { InvalidReaderPasswordException } from '../errors/invalid-reader-password-exception.error';
+import { BcryptProvider } from '@/app/interfaces/hash/bcrypt.provider';
 
 describe('AuthenticateReaderService', () => {
   let service: AuthenticateReaderService;
   let readersRepository: jest.Mocked<ReadersRepository>;
+  let bcryptProvider: jest.Mocked<BcryptProvider>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,12 +24,20 @@ describe('AuthenticateReaderService', () => {
             create: jest.fn(),
           })),
         },
+        {
+          provide: BcryptProvider,
+          useClass: jest.fn().mockImplementation(() => ({
+            compare: jest.fn(),
+            hash: jest.fn(),
+          })),
+        },
         AuthenticateReaderService
       ],
     }).compile();
 
     service = module.get<AuthenticateReaderService>(AuthenticateReaderService);
     readersRepository = module.get<jest.Mocked<ReadersRepository>>(ReadersRepository);
+    bcryptProvider = module.get<jest.Mocked<BcryptProvider>>(BcryptProvider);
   });
 
   it('should throw ReaderNotFoundException when reader does not exist', async () => {
@@ -44,6 +56,35 @@ describe('AuthenticateReaderService', () => {
     await expect(promise).rejects.toThrow(ReaderNotFoundException);
   });
 
-  it.todo('should throw InvalidReaderPasswordException when password does not match');
+  it('should throw InvalidReaderPasswordException when password does not match', async () => {
+    // Arrange
+    const params = {
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 12 }),
+    };
+
+    const mockReader = new Reader({
+      id: faker.number.int(),
+      name: faker.person.fullName(),
+      email: params.email,
+      password: faker.internet.password({ length: 12 }),
+      createdAt: faker.date.recent(),
+      updatedAt: null,
+      deletedAt: null,
+      deleted: false,
+    });
+
+    readersRepository.getByEmail.mockResolvedValue(mockReader);
+
+
+    bcryptProvider.compare.mockResolvedValue(params.password === mockReader.password);
+
+    // Act
+    const promise = service.execute(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(InvalidReaderPasswordException);
+  });
+
   it.todo('should return a valid token when reader is authenticated');
 });
