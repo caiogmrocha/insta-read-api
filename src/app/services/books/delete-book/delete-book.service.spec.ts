527f9511f6c34b346c29dbe0f13c239d4ab21c6d
caiogmrocha <caioguilherme739@gmail.com>
2024-06-28 18:41:28 -0300

    test(LIB-10): ensure DeleteBookService throw an error if the book does not exist

--- src/app/services/books/delete-book/delete-book.service.spec.ts
+++ src/app/services/books/delete-book/delete-book.service.spec.ts
@@ -1,17 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DeleteBookService } from './delete-book.service';
+import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
+import { BookNotFoundException } from '../errors/book-not-found.exception';
 
 describe('DeleteBookService', () => {
   let service: DeleteBookService;
+  let booksRepository: jest.Mocked<BooksRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DeleteBookService],
+      providers: [
+        {
+          provide: BooksRepository,
+          useClass: jest.fn().mockImplementation(() => ({
+            getById: jest.fn(),
+            delete: jest.fn(),
+          })),
+        },
+        DeleteBookService,
+      ],
     }).compile();
 
     service = module.get<DeleteBookService>(DeleteBookService);
+    booksRepository = module.get(BooksRepository);
   });
 
-  it.todo('should throw an error if the book does not exist');
+  it('should throw an error if the book does not exist', async () => {
+    // Arrange
+    const bookId = 1;
+
+    booksRepository.getById.mockResolvedValue(null);
+
+    // Act
+    const result = service.execute({ id: bookId });
+
+    // Assert
+    await expect(result).rejects.toThrow(BookNotFoundException);
+  });
+
   it.todo('should delete a book');
 });
