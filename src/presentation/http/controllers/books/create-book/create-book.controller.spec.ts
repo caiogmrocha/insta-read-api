55c360c02db6c1ddfc56d2835f0309a2eeaa16b0
caiogmrocha <caioguilherme739@gmail.com>
2024-06-28 00:13:53 -0300

    test(LIB-7): ensure CreateBookController response with 409 status code when ISBN already exists

--- src/presentation/http/controllers/books/create-book/create-book.controller.spec.ts
+++ src/presentation/http/controllers/books/create-book/create-book.controller.spec.ts
@@ -3,6 +3,8 @@ import { CreateBookController } from './create-book.controller';
 import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
 import { CreateBookService } from '@/app/services/books/create-book/create-book.service';
 import { faker } from '@faker-js/faker';
+import { Book } from '@/domain/entities/book';
+import { ConflictException } from '@nestjs/common';
 
 describe('CreateBookController', () => {
   let controller: CreateBookController;
@@ -49,6 +51,27 @@ describe('CreateBookController', () => {
     await expect(promise).resolves.toBeUndefined();
   });
 
-  it.todo('should response with 400 status code when ISBN already exists');
+  it('should response with 409 status code when ISBN already exists', async () => {
+    // Arrange
+    const params = {
+      isbn: faker.helpers.replaceSymbols('###-#-######-##-#'),
+      title: faker.commerce.productName(),
+      sinopsis: faker.lorem.paragraph(),
+      pages: faker.number.int({ min: 50, max: 500 }),
+      author: faker.person.fullName(),
+      category: faker.commerce.department(),
+      publisher: faker.company.name(),
+      publicationDate: faker.date.past(),
+    };
+
+    booksRepository.getByISBN.mockResolvedValue(new Book(params));
+
+    // Act
+    const promise = controller.handle(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(ConflictException);
+  });
+
   it.todo('should response with 500 status code when an unexpected error occurs');
 });
