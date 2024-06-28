9a563ba02926705e3298945885c7ede914721305
caiogmrocha <caioguilherme739@gmail.com>
2024-06-28 16:07:00 -0300

    test(LIB-11): ensure GetPaginatedBooksService return a list of books only with specific fields

--- src/app/services/books/get-paginated-books/get-paginated-books.service.spec.ts
+++ src/app/services/books/get-paginated-books/get-paginated-books.service.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { GetPaginatedBooksService } from './get-paginated-books.service';
+import { GetPaginatedBooksService, GetPaginatedBooksServiceParams } from './get-paginated-books.service';
 import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
 import { faker } from '@faker-js/faker';
 import { Book } from '@/domain/entities/book';
+
+export let bookRepositoryGetPaginatedMock: jest.Mock;
 
 describe('GetPaginatedBooksService', () => {
   let service: GetPaginatedBooksService;
@@ -19,18 +21,36 @@ describe('GetPaginatedBooksService', () => {
     publicationDate: faker.date.past(),
   }), { count: 20 })
 
+  bookRepositoryGetPaginatedMock = jest.fn((params: Parameters<BooksRepository['getPaginated']>[0]) => {
+    let processedBooks = mockedBooks.slice((params.page - 1) * params.limit, params.page * params.limit);
+
+    if (params.fields) {
+      processedBooks = processedBooks.map((book) => {
+        const bookProps = Object.entries(book);
+
+        const filteredProps = bookProps.filter(([key]) => params.fields.includes(key as keyof Book));
+
+        const filteredBook = Object.fromEntries(filteredProps);
+
+        const newBook = new Book(filteredBook);
+
+        return newBook;
+      });
+    }
+
+    return Promise.resolve({
+      data: processedBooks,
+      total: mockedBooks.length,
+    });
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: BooksRepository,
           useClass: jest.fn().mockImplementation(() => ({
-            getPaginated: jest.fn((params: Parameters<BooksRepository['getPaginated']>[0]) => {
-              return Promise.resolve({
-                data: mockedBooks.slice((params.page - 1) * params.limit, params.page * params.limit),
-                total: mockedBooks.length,
-              });
-            }),
+            getPaginated: bookRepositoryGetPaginatedMock,
           })),
         },
         GetPaginatedBooksService,
@@ -76,5 +96,34 @@ describe('GetPaginatedBooksService', () => {
     expect(result.data).toHaveLength(10);
   });
 
-  it.todo('should return a list of books only with specific fields');
+  it('should return a list of books only with specific fields', async () => {
+    // Arrange
+    const params: GetPaginatedBooksServiceParams = {
+      page: 1,
+      limit: 10,
+      fields: ['title', 'author'],
+    };
+
+    // Act
+    const result = await service.execute(params);
+
+    // Assert
+    expect(result).toEqual({
+      data: expect.arrayContaining([
+        expect.objectContaining({
+          title: expect.any(String),
+          author: expect.any(String),
+        }),
+      ]),
+      total: mockedBooks.length,
+    });
+    expect(result.data[0]).not.toHaveProperty('isbn');
+    expect(result.data[0]).not.toHaveProperty('sinopsis');
+    expect(result.data[0]).not.toHaveProperty('pages');
+    expect(result.data[0]).not.toHaveProperty('amount');
+    expect(result.data[0]).not.toHaveProperty('category');
+    expect(result.data[0]).not.toHaveProperty('publisher');
+    expect(result.data[0]).not.toHaveProperty('publicationDate');
+    expect(result.data).toHaveLength(10);
+  });
 });
