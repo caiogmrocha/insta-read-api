1f57a67c171f4ef5feb79eba1c19dd30a8f3b1ba
caiogmrocha <caioguilherme739@gmail.com>
2024-06-29 18:30:16 -0300

    test(LIB-8): ensure UpdateBookController response with 409 when ISBN already exists

--- src/presentation/http/controllers/books/update-book/update-book.controller.spec.ts
+++ src/presentation/http/controllers/books/update-book/update-book.controller.spec.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { faker } from '@faker-js/faker';
@@ -7,6 +7,7 @@ import { UpdateBookController } from './update-book.controller';
 import { UpdateBookBodyDto, UpdateBookParamsDto } from './update-book.dto';
 import { UpdateBookService } from '@/app/services/books/update-book/update-book.service';
 import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
+import { Book } from '@/domain/entities/book';
 
 describe('UpdateBookController', () => {
   let controller: UpdateBookController;
@@ -61,6 +62,54 @@ describe('UpdateBookController', () => {
     await expect(promise).rejects.toThrow(NotFoundException);
   });
 
-  it.todo('should response with 409 when ISBN already exists');
+  it('should response with 409 when ISBN already exists', async () => {
+    // Arrange
+    const params: UpdateBookParamsDto = {
+      id: faker.number.int(),
+    };
+
+    const body: UpdateBookBodyDto = {
+      isbn: faker.helpers.replaceSymbols('###-#-##-#####-#'),
+      title: faker.lorem.words(),
+      sinopsis: faker.lorem.paragraph(),
+      pages: faker.number.int(),
+      amount: faker.number.int(),
+      author: faker.person.fullName(),
+      category: faker.lorem.word(),
+      publisher: faker.company.name(),
+      publicationDate: faker.date.recent(),
+    };
+
+    const bookToBeUpdated = new Book({
+      ...params,
+      ...body,
+    });
+
+    booksRepository.getById.mockResolvedValue(bookToBeUpdated);
+
+    body.isbn = faker.helpers.replaceSymbols('###-#-##-#####-#');
+
+    const bookThatAlreadyExists = new Book({
+      id: faker.number.int(),
+      isbn: body.isbn,
+      title: faker.lorem.words(),
+      sinopsis: faker.lorem.paragraph(),
+      pages: faker.number.int(),
+      amount: faker.number.int(),
+      author: faker.person.fullName(),
+      category: faker.lorem.word(),
+      publisher: faker.company.name(),
+      publicationDate: faker.date.recent(),
+    });
+
+    booksRepository.getByISBN.mockResolvedValue(bookThatAlreadyExists);
+
+    // Act
+    const promise = controller.handle(params, body);
+
+    // Assert
+    await expect(promise).rejects.toThrow(ConflictException);
+  });
+
   it.todo('should response with 500 when any error happens');
 });
