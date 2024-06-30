77171a5dff39a3684e10fa65314106a5bd50c9dc
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 14:40:59 -0300

    test(LIB-3): ensure UpdateReaderAccountService throw ReaderEmailAlreadyExistsException when email already exists

--- src/app/services/readers/update-reader-account/update-reader-account.service.spec.ts
+++ src/app/services/readers/update-reader-account/update-reader-account.service.spec.ts
@@ -3,6 +3,8 @@ import { UpdateReaderAccountService, UpdateReaderAccountServiceParams } from './
 import { ReaderNotFoundException } from '../errors/reader-not-found.exception';
 import { faker } from '@faker-js/faker';
 import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
+import { Reader } from '@/domain/entities/reader';
+import { ReaderEmailAlreadyExistsException } from '../errors/reader-email-already-exists.exception';
 
 describe('UpdateReaderAccountService', () => {
   let service: UpdateReaderAccountService;
@@ -15,6 +17,7 @@ describe('UpdateReaderAccountService', () => {
           provide: ReadersRepository,
           useClass: jest.fn().mockImplementation((): jest.Mocked<Partial<ReadersRepository>> => ({
             getById: jest.fn(),
+            getByEmail: jest.fn(),
             update: jest.fn(),
           })),
         },
@@ -44,6 +47,41 @@ describe('UpdateReaderAccountService', () => {
     await expect(promise).rejects.toThrow(ReaderNotFoundException);
   });
 
-  it.todo('should throw ReaderEmailAlreadyExistsException when email already exists');
+  it('should throw ReaderEmailAlreadyExistsException when email already exists', async () => {
+    // Arrange
+    const params: UpdateReaderAccountServiceParams = {
+      id: faker.number.int(),
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const readerToBeUpdated = new Reader({
+      id: params.id,
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    });
+
+    readersRepository.getById.mockResolvedValue(readerToBeUpdated);
+
+    params.email = faker.internet.email();
+
+    const readerWithSameEmail = new Reader({
+      id: faker.number.int(),
+      name: faker.person.fullName(),
+      email: params.email,
+      password: faker.internet.password({ length: 12 }),
+    });
+
+    readersRepository.getByEmail.mockResolvedValue(readerWithSameEmail);
+
+    // Act
+    const promise = service.execute(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(ReaderEmailAlreadyExistsException);
+  });
+
   it.todo('should update the reader account');
 });
