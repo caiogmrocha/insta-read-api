a05b6b96c0be9def105dc5cf4af8a4b0785d15f1
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 14:44:00 -0300

    feat(LIB-3): ensure UpdateReaderAccountService update the reader account

--- src/app/interfaces/repositories/reader.repository.ts
+++ src/app/interfaces/repositories/reader.repository.ts
@@ -1,7 +1,7 @@
 import { Reader } from "src/domain/entities/reader";
 
 export interface ReadersRepository {
-  getById(id: string): Promise<Reader | null>;
+  getById(id: number): Promise<Reader | null>;
   getByEmail(email: string): Promise<Reader | null>;
   create(params: Reader): Promise<void>;
   update(params: Reader): Promise<void>;
--- src/app/services/readers/update-reader-account/update-reader-account.service.ts
+++ src/app/services/readers/update-reader-account/update-reader-account.service.ts
@@ -3,12 +3,12 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ReaderNotFoundException } from '../errors/reader-not-found.exception';
 import { ReaderEmailAlreadyExistsException } from '../errors/reader-email-already-exists.exception';
 
-export type UpdateReaderAccountServiceParams = {
+export type UpdateReaderAccountServiceParams = Partial<{
   id: number;
   name: string;
   email: string;
   password: string;
-};
+}>;
 
 @Injectable()
 export class UpdateReaderAccountService {
@@ -17,7 +17,7 @@ export class UpdateReaderAccountService {
   ) {}
 
   public async execute(params: UpdateReaderAccountServiceParams): Promise<void> {
-    const reader = await this.readersRepository.getById(params.email);
+    const reader = await this.readersRepository.getById(params.id);
 
     if (!reader) {
       throw new ReaderNotFoundException('id', params.id);
@@ -30,5 +30,9 @@ export class UpdateReaderAccountService {
         throw new ReaderEmailAlreadyExistsException(params.email);
       }
     }
+
+    Object.assign(reader, params);
+
+    await this.readersRepository.update(reader);
   }
 }
