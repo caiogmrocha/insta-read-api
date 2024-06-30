a621f59d36fd2255e4349786f7ca29d19519f537
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 16:47:43 -0300

    test(LIB-9): ensure ArchiveReaderAccountService throw ReaderNotFound exception if reader not found

--- src/app/services/readers/archive-reader-account/archive-reader-account.service.spec.ts
+++ src/app/services/readers/archive-reader-account/archive-reader-account.service.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { ArchiveReaderAccountService } from './archive-reader-account.service';
+import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
+import { faker } from '@faker-js/faker';
+import { ReaderNotFoundException } from '../errors/reader-not-found.exception';
 
 describe('ArchiveReaderAccountService', () => {
   let service: ArchiveReaderAccountService;
+  let readersRepository: jest.Mocked<ReadersRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArchiveReaderAccountService],
+      providers: [
+        {
+          provide: ReadersRepository,
+          useClass: jest.fn().mockImplementation(() => ({
+            getById: jest.fn(),
+            update: jest.fn(),
+          })),
+        },
+        ArchiveReaderAccountService,
+      ],
     }).compile();
 
     service = module.get<ArchiveReaderAccountService>(ArchiveReaderAccountService);
+    readersRepository = module.get<jest.Mocked<ReadersRepository>>(ReadersRepository);
   });
 
-  it.todo('should throw ReaderNotFound exception if reader not found');
+  it('should throw ReaderNotFound exception if reader not found', async () => {
+    // Arrange
+    const params = {
+      id: faker.number.int(),
+    };
+
+    readersRepository.getById.mockResolvedValue(null);
+
+    // Act
+    const promise = service.execute(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(ReaderNotFoundException);
+  });
+
   it.todo('should throw ReaderAlreadyArchived exception if reader already archived');
   it.todo('should archive reader account');
 });
