97e31b0fd2567d68eaa89d6c65a53fbc232bd3dc
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 15:10:50 -0300

    feat(LIB-3): ensure UpdateReaderAccountController response with 404 when reader not found

--- src/presentation/http/controllers/readers/update-reader-account/update-reader-account.controller.ts
+++ src/presentation/http/controllers/readers/update-reader-account/update-reader-account.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { ReaderNotFoundException } from '@/app/services/readers/errors/reader-not-found.exception';
+import { UpdateReaderAccountService } from '@/app/services/readers/update-reader-account/update-reader-account.service';
+import { Body, Controller, InternalServerErrorException, NotFoundException, Param, Put } from '@nestjs/common';
+import { UpdateReaderAccountBodyDto, UpdateReaderAccountParamsDto } from './update-reader-account.dto';
 
-@Controller('update-reader-account')
-export class UpdateReaderAccountController {}
+@Controller()
+export class UpdateReaderAccountController {
+  constructor (
+    private readonly updateReaderAccountService: UpdateReaderAccountService,
+  ) {}
+
+  @Put('/api/readers/:id')
+  public async handle(
+    @Param() params: UpdateReaderAccountParamsDto,
+    @Body() body: UpdateReaderAccountBodyDto,
+  ): Promise<void> {
+    try {
+      await this.updateReaderAccountService.execute({
+        ...params,
+        ...body,
+      });
+    } catch (error) {
+      switch (error.constructor) {
+        case ReaderNotFoundException: {
+          throw new NotFoundException('Reader not found');
+        };
+
+        default: {
+          throw new InternalServerErrorException(error.message);
+        };
+      }
+    }
+  }
+}
