77b435ddf98fb95287ad69061ff463d134dc71de
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 14:36:08 -0300

    feat(LIB-3): ensure UpdateReaderAccountService throw ReaderNotFoundException when reader not found

--- src/app/services/readers/update-reader-account/update-reader-account.service.ts
+++ src/app/services/readers/update-reader-account/update-reader-account.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
+import { Inject, Injectable } from '@nestjs/common';
+import { ReaderNotFoundException } from '../errors/reader-not-found.exception';
+
+export type UpdateReaderAccountServiceParams = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+};
 
 @Injectable()
-export class UpdateReaderAccountService {}
+export class UpdateReaderAccountService {
+  constructor (
+    @Inject(ReadersRepository) private readonly readersRepository: ReadersRepository,
+  ) {}
+
+  public async execute(params: UpdateReaderAccountServiceParams): Promise<void> {
+    const reader = await this.readersRepository.getById(params.email);
+
+    if (!reader) {
+      throw new ReaderNotFoundException('id', params.id);
+    }
+  }
+}
