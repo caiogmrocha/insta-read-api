e9cbaffead7af7c92f5491c3400b4767315fe391
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 16:47:38 -0300

    feat(LIB-9): ensure ArchiveReaderAccountService throw ReaderNotFound exception if reader not found

--- src/app/services/readers/archive-reader-account/archive-reader-account.service.ts
+++ src/app/services/readers/archive-reader-account/archive-reader-account.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
+import { Inject, Injectable } from '@nestjs/common';
+import { ReaderNotFoundException } from '../errors/reader-not-found.exception';
+
+export type ArchiveReaderAccountServiceParams = {
+  id: number;
+};
 
 @Injectable()
-export class ArchiveReaderAccountService {}
+export class ArchiveReaderAccountService {
+  constructor (
+    @Inject(ReadersRepository) private readonly readersRepository: ReadersRepository,
+  ) {}
+
+  public async execute(params: ArchiveReaderAccountServiceParams): Promise<void> {
+    const reader = await this.readersRepository.getById(params.id);
+
+    if (!reader) {
+      throw new ReaderNotFoundException('id', params.id);
+    }
+  }
+}
