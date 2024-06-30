4bec00e028a8aab7604de565443040ef96be8ef3
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 14:36:14 -0300

    test(LIB-3): ensure UpdateReaderAccountService throw ReaderNotFoundException when reader not found

--- src/app/services/readers/update-reader-account/update-reader-account.service.spec.ts
+++ src/app/services/readers/update-reader-account/update-reader-account.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UpdateReaderAccountService } from './update-reader-account.service';
+import { UpdateReaderAccountService, UpdateReaderAccountServiceParams } from './update-reader-account.service';
+import { ReaderNotFoundException } from '../errors/reader-not-found.exception';
+import { faker } from '@faker-js/faker';
+import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
 
 describe('UpdateReaderAccountService', () => {
   let service: UpdateReaderAccountService;
+  let readersRepository: jest.Mocked<ReadersRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UpdateReaderAccountService],
+      providers: [
+        {
+          provide: ReadersRepository,
+          useClass: jest.fn().mockImplementation((): jest.Mocked<Partial<ReadersRepository>> => ({
+            getById: jest.fn(),
+            update: jest.fn(),
+          })),
+        },
+        UpdateReaderAccountService,
+      ],
     }).compile();
 
     service = module.get<UpdateReaderAccountService>(UpdateReaderAccountService);
+    readersRepository = module.get<jest.Mocked<ReadersRepository>>(ReadersRepository);
   });
 
-  it.todo('should throw ReaderNotFoundException when reader not found');
+  it('should throw ReaderNotFoundException when reader not found', async () => {
+    // Arrange
+    const params: UpdateReaderAccountServiceParams = {
+      id: faker.number.int(),
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    readersRepository.getById.mockResolvedValue(null);
+
+    // Act
+    const promise = service.execute(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(ReaderNotFoundException);
+  });
+
   it.todo('should throw ReaderEmailAlreadyExistsException when email already exists');
   it.todo('should update the reader account');
 });
