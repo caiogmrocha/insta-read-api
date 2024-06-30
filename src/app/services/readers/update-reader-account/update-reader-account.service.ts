fd8f11f7587c1200ee67e8d082cca25da47ba5bc
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 14:40:54 -0300

    feat(LIB-3): ensure UpdateReaderAccountService throw ReaderEmailAlreadyExistsException when email already exists

--- src/app/services/readers/update-reader-account/update-reader-account.service.ts
+++ src/app/services/readers/update-reader-account/update-reader-account.service.ts
@@ -1,6 +1,7 @@
 import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
 import { Inject, Injectable } from '@nestjs/common';
 import { ReaderNotFoundException } from '../errors/reader-not-found.exception';
+import { ReaderEmailAlreadyExistsException } from '../errors/reader-email-already-exists.exception';
 
 export type UpdateReaderAccountServiceParams = {
   id: number;
@@ -20,6 +21,14 @@ export class UpdateReaderAccountService {
 
     if (!reader) {
       throw new ReaderNotFoundException('id', params.id);
+    }
+
+    if (reader.email !== params.email) {
+      const readerWithEmail = await this.readersRepository.getByEmail(params.email);
+
+      if (readerWithEmail) {
+        throw new ReaderEmailAlreadyExistsException(params.email);
+      }
     }
   }
 }
