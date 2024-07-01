e98e363b696a0405f6fd9992438d5c518cdcb592
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 23:48:36 -0300

    feat(LIB-5): ensure RequestBookLoanService throw BookNotFoundException when book is not found

--- src/app/services/loans/request-book-loan/request-book-loan.service.ts
+++ src/app/services/loans/request-book-loan/request-book-loan.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
+import { Inject, Injectable } from '@nestjs/common';
+import { BookNotFoundException } from '../../books/errors/book-not-found.exception';
+
+export type RequestBookLoanParams = {
+  readerId: number;
+  bookId: number;
+};
 
 @Injectable()
-export class RequestBookLoanService {}
+export class RequestBookLoanService {
+  constructor (
+    @Inject(BooksRepository) private readonly booksRepository: BooksRepository,
+  ) {}
+
+  public async execute(params: RequestBookLoanParams): Promise<void> {
+    const book = await this.booksRepository.getById(params.bookId);
+
+    if (!book) {
+      throw new BookNotFoundException('id', params.bookId);
+    }
+  }
+}
