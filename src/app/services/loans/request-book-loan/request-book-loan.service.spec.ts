90696eb1b73684acbe8d732bb8cc95732027dffd
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 23:48:41 -0300

    test(LIB-5): ensure RequestBookLoanService throw BookNotFoundException when book is not found

--- src/app/services/loans/request-book-loan/request-book-loan.service.spec.ts
+++ src/app/services/loans/request-book-loan/request-book-loan.service.spec.ts
@@ -1,19 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { RequestBookLoanService } from './request-book-loan.service';
+import { RequestBookLoanParams, RequestBookLoanService } from './request-book-loan.service';
+import { faker } from '@faker-js/faker';
+import { BookNotFoundException } from '../../books/errors/book-not-found.exception';
+import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
 
 describe('RequestBookLoanService', () => {
   let service: RequestBookLoanService;
+  let booksRepository: jest.Mocked<BooksRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RequestBookLoanService],
+      providers: [
+        {
+          provide: BooksRepository,
+          useClass: jest.fn().mockImplementation(() => ({
+            getById: jest.fn(),
+          })),
+        },
+        RequestBookLoanService,
+      ],
     }).compile();
 
     service = module.get<RequestBookLoanService>(RequestBookLoanService);
+    booksRepository = module.get(BooksRepository);
   });
 
-  it.todo('should throw BookNotFoundException when book is not found');
+  it('should throw BookNotFoundException when book is not found', async () => {
+    // Arrange
+    const params: RequestBookLoanParams = {
+      readerId: faker.number.int(),
+      bookId: faker.number.int(),
+    };
+
+    booksRepository.getById.mockResolvedValue(null);
+
+    // Act
+    const promise = service.execute(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(BookNotFoundException);
+  });
+
   it.todo('should throw ReaderNotFoundException when reader is not found');
-  it.skip('should throw ReaderNotActiveException when reader is not active'); // This test is not necessary
+  it.todo('should throw ReaderNotActiveException when reader is not active'); // This test is not necessary
   it.todo('should enqueue loan request');
 });
