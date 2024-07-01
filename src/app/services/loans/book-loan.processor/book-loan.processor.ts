1a377f2dceaaa946755eeda43825bfa649e4e295
caiogmrocha <caioguilherme739@gmail.com>
2024-07-01 14:02:57 -0300

    feat(LIB-5): ensure BookLoanProcessor throw BookNotFoundException when book is not found

--- src/app/services/loans/book-loan.processor/book-loan.processor.ts
+++ src/app/services/loans/book-loan.processor/book-loan.processor.ts
@@ -1,9 +1,29 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Inject } from '@nestjs/common';
+
 import { Job } from 'bullmq';
+
+import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
+import { BookNotFoundException } from '../../books/errors/book-not-found.exception';
+
+export type BookLoanParams = {
+  readerId: number;
+  bookId: number;
+};
 
 @Processor('book-loan')
 export class BookLoanProcessor extends WorkerHost {
-  public async process(job: Job): Promise<any> {
+  constructor (
+    @Inject(BooksRepository) private readonly booksRepository: BooksRepository,
+  ) { super() }
 
+  public async process(job: Job<BookLoanParams>): Promise<any> {
+    const { readerId, bookId } = job.data;
+
+    const book = await this.booksRepository.getById(bookId);
+
+    if (!book) {
+      throw new BookNotFoundException('id', bookId);
+    }
   }
 }
