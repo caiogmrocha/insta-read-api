8d093d2aab1ed95d6190c4df144e132fb9161c8d
caiogmrocha <caioguilherme739@gmail.com>
2024-06-30 23:52:13 -0300

    test(LIB-5): ensure RequestBookLoanService throw ReaderNotFoundException when reader is not found

--- src/app/services/loans/request-book-loan/request-book-loan.service.spec.ts
+++ src/app/services/loans/request-book-loan/request-book-loan.service.spec.ts
@@ -3,10 +3,14 @@ import { RequestBookLoanParams, RequestBookLoanService } from './request-book-lo
 import { faker } from '@faker-js/faker';
 import { BookNotFoundException } from '../../books/errors/book-not-found.exception';
 import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
+import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
+import { Book } from '@/domain/entities/book';
+import { ReaderNotFoundException } from '../../readers/errors/reader-not-found.exception';
 
 describe('RequestBookLoanService', () => {
   let service: RequestBookLoanService;
   let booksRepository: jest.Mocked<BooksRepository>;
+  let readersRepository: jest.Mocked<ReadersRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,12 +21,19 @@ describe('RequestBookLoanService', () => {
             getById: jest.fn(),
           })),
         },
+        {
+          provide: ReadersRepository,
+          useClass: jest.fn().mockImplementation(() => ({
+            getById: jest.fn(),
+          })),
+        },
         RequestBookLoanService,
       ],
     }).compile();
 
     service = module.get<RequestBookLoanService>(RequestBookLoanService);
     booksRepository = module.get(BooksRepository);
+    readersRepository = module.get(ReadersRepository);
   });
 
   it('should throw BookNotFoundException when book is not found', async () => {
@@ -41,7 +52,28 @@ describe('RequestBookLoanService', () => {
     await expect(promise).rejects.toThrow(BookNotFoundException);
   });
 
-  it.todo('should throw ReaderNotFoundException when reader is not found');
+  it('should throw ReaderNotFoundException when reader is not found', async () => {
+    // Arrange
+    const params: RequestBookLoanParams = {
+      readerId: faker.number.int(),
+      bookId: faker.number.int(),
+    };
+
+    const book = new Book({
+      id: params.bookId,
+      title: faker.lorem.words(),
+    });
+
+    booksRepository.getById.mockResolvedValue(book);
+    readersRepository.getById.mockResolvedValue(null);
+
+    // Act
+    const promise = service.execute(params);
+
+    // Assert
+    await expect(promise).rejects.toThrow(ReaderNotFoundException);
+  });
+
   it.todo('should throw ReaderNotActiveException when reader is not active'); // This test is not necessary
   it.todo('should enqueue loan request');
 });
