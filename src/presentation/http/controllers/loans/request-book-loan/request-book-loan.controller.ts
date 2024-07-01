12370619670154c2b7898eeddd5bdd44be920d0f
caiogmrocha <caioguilherme739@gmail.com>
2024-07-01 01:11:45 -0300

    feat(LIB-5): ensure RequestBookLoanController  response with 404 status code when book is not found

--- src/presentation/http/controllers/loans/request-book-loan/request-book-loan.controller.ts
+++ src/presentation/http/controllers/loans/request-book-loan/request-book-loan.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, InternalServerErrorException, NotFoundException, Post } from '@nestjs/common';
 
-@Controller('request-book-loan')
-export class RequestBookLoanController {}
+import { RequestBookLoanBodyDto } from './request-book-loan.dto';
+import { RequestBookLoanService } from '@/app/services/loans/request-book-loan/request-book-loan.service';
+import { BookNotFoundException } from '@/app/services/books/errors/book-not-found.exception';
+
+@Controller()
+export class RequestBookLoanController {
+  constructor (
+    private readonly requestBookLoanService: RequestBookLoanService,
+  ) {}
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Post('/api/loans/request-book-loan')
+  public async handle(@Body() body: RequestBookLoanBodyDto): Promise<void> {
+    try {
+      await this.requestBookLoanService.execute(body);
+    } catch (error) {
+      switch (error.constructor) {
+        case BookNotFoundException: {
+          throw new NotFoundException();
+        };
+
+        default: {
+          throw new InternalServerErrorException();
+        };
+      }
+    }
+  }
+}
