33a3399451b87ea827f59558c98622bfa38aedcd
caiogmrocha <caioguilherme739@gmail.com>
2024-07-02 20:00:32 -0300

    test(LIB-5): ensure BookLoanProcessor notify reader when book loan request is successful

--- src/app/services/loans/book-loan.processor/book-loan.processor.spec.ts
+++ src/app/services/loans/book-loan.processor/book-loan.processor.spec.ts
@@ -6,15 +6,18 @@ import { Job } from 'bullmq';
 import { BookLoanParams, BookLoanProcessor } from './book-loan.processor';
 import { BooksRepository } from '@/app/interfaces/repositories/books.repository';
 import { ReadersRepository } from '@/app/interfaces/repositories/reader.repository';
+import { WebSocketProvider } from '@/app/interfaces/websockets/websocket-server.provider';
 import { BookNotFoundException } from '../../books/errors/book-not-found.exception';
 import { ReaderNotFoundException } from '../../readers/errors/reader-not-found.exception';
-import { Reader } from '@/domain/entities/reader';
 import { ReaderAccountDeactivatedException } from '../../readers/errors/reader-account-deactivated.exception';
+import { Reader } from '@/domain/entities/reader';
+import { Book } from '@/domain/entities/book';
 
 describe('BookLoanProcessor', () => {
   let service: BookLoanProcessor;
   let booksRepository: jest.Mocked<BooksRepository>;
   let readersRepository: jest.Mocked<ReadersRepository>;
+  let webSocketProvider: jest.Mocked<WebSocketProvider>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,6 +34,12 @@ describe('BookLoanProcessor', () => {
             getById: jest.fn(),
           })),
         },
+        {
+          provide: WebSocketProvider,
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
         BookLoanProcessor,
       ],
     }).compile();
@@ -38,6 +47,7 @@ describe('BookLoanProcessor', () => {
     service = module.get<BookLoanProcessor>(BookLoanProcessor);
     booksRepository = module.get<jest.Mocked<BooksRepository>>(BooksRepository);
     readersRepository = module.get<jest.Mocked<ReadersRepository>>(ReadersRepository);
+    webSocketProvider = module.get<jest.Mocked<WebSocketProvider>>(WebSocketProvider);
   });
 
   it('should throw BookNotFoundException when book is not found', async () => {
@@ -104,5 +114,42 @@ describe('BookLoanProcessor', () => {
     await expect(promise).rejects.toThrow(ReaderAccountDeactivatedException);
   });
 
-  it.todo('should notify reader when book loan request is successful');
+  it('should notify reader when book loan request is successful', async () => {
+    // Arrange
+    const params = {
+      data: {
+        readerId: faker.number.int(),
+        bookId: faker.number.int(),
+      },
+    } as Job<BookLoanParams>;
+
+    const book = new Book({
+      id: params.data.bookId,
+      title: faker.lorem.words(),
+    });
+
+    booksRepository.getById.mockResolvedValue(book);
+
+    const reader = new Reader({
+      id: params.data.readerId,
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      isArchived: false,
+    });
+
+    readersRepository.getById.mockResolvedValue(reader);
+
+    // Act
+    const promise = service.process(params);
+
+    // Assert
+    await expect(promise).resolves.not.toThrow();
+    expect(webSocketProvider.emit).toHaveBeenCalledWith('notify', reader.id, {
+      type: 'book-loan',
+      data: {
+        bookId: book.id,
+        bookTitle: book.title,
+      },
+    });
+  });
 });
